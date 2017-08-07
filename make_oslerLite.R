e68abacdd71b512cbaf0513ebb550aa615491f98
muschellij2 <muschellij2@gmail.com>
2017-08-07 13:20:59 -0400

    updating make_oslerLite

--- make_oslerLite.R
+++ make_oslerLite.R
@@ -3,10 +3,10 @@
 #####################
 version = read.dcf("DESCRIPTION")[, "Version"]
 # header
-top = readLines("neuro_top.R")
+top = readLines("osler_top.R")
 
-# neurocLite definition
-content = readLines("R/neuro_install.R")
+# oslerLite definition
+content = readLines("R/osler_install.R")
 content = paste0("\t", content)
 
 vc = readLines("R/make_full_version.R")
@@ -14,16 +14,16 @@ vc = paste0("\t", vc)
 content = c(content, vc)
 
 # adding version to the head and the printout
-version = paste0("# neurocInstall package version: ", version)
+version = paste0("# oslerInstall package version: ", version)
 version = c(version, paste0("pkg_ver = '", version, "'"))
 out = c(version, top, content)
 
-# adding neuro_package tables so that things work
-content = readLines("R/neuro_package_table.R")
+# adding osler_package tables so that things work
+content = readLines("R/osler_package_table.R")
 content = paste0("\t", content)
 
 out = c(out, "", content)
 # adding bottom of if statement
-bot = readLines("neuro_bottom.R")
+bot = readLines("osler_bottom.R")
 out = c(out, bot)
-writeLines(text = out, con = "neurocLite.R")
+writeLines(text = out, con = "oslerLite.R")
